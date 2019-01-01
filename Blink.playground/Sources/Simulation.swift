77712cd673aa4b2df34ffdbcf40556cc21a026d1
송동욱 <quino0627@korea.ac.kr>
2019-01-02 01:04:55 +0900

    fixed old syntax

--- Blink.playground/Sources/Simulation.swift
+++ Blink.playground/Sources/Simulation.swift
@@ -1,4 +1,4 @@
-// 
+//
 //  Simulation.swift
 //
 //  Copyright (c) 2016 Apple Inc. All Rights Reserved.
@@ -13,7 +13,7 @@ public enum Pattern {
 }
 
 /**
-    A class to simulate a collection of cells. You can set a color, image, or text to customize your cell's states (alive, dead, or idle).
+ A class to simulate a collection of cells. You can set a color, image, or text to customize your cell's states (alive, dead, or idle).
  
  - `cellDimension`: The width and height for each cell from 4 to 512.
  - `speed`: How many times per second the simulation updates.
@@ -21,7 +21,7 @@ public enum Pattern {
  - `set(_ image: UIImage, forState state: State)`: Displays an image on all cells of the chosen state.
  - `set(_ text: String, forState state: State)`: Displays an emoji or letters on all cells of the chosen state.
  - `set(_ color: UIColor, forState state: State)`: Changes the color for all cells of the chosen state.
-
+ 
  - `placePattern(_ pattern: Pattern, atColumn column: Int, row: Int, state: State = .alive)`: Places different patterns of cells onto the grid, starting at the specified row and column. These cells will be in the specified state.
  - `setState(_ state: State, atColumn column: Int, row: Int)` Sets the initial state for a cell at a particular row and column.
  */
@@ -34,7 +34,7 @@ public class Simulation {
             cellConfigurator.gridLineThickness = gridLineThickness
         }
     }
-
+    
     public var gridColor: UIColor? {
         didSet {
             cellConfigurator.gridColor = gridColor
@@ -78,17 +78,17 @@ public class Simulation {
                      withOffset: (column, row))
         }
     }
-
+    
     var initialState = [Coordinate: State]()
     
     var isPaused: Bool = false
     
     /*
-     Note: This name does not conform to the proper naming within the project, but 
-     rather is adjusted for the perspective of the playground user. Currently it 
-     only effects the initial state and does not modify anything once the simulation 
+     Note: This name does not conform to the proper naming within the project, but
+     rather is adjusted for the perspective of the playground user. Currently it
+     only effects the initial state and does not modify anything once the simulation
      is running.
-    */
+     */
     /// Sets the initial state for a cell at a particular row and column.
     public func setState(_ state: State, atColumn column: Int, row: Int ) {
         let coordinate = Coordinate(column: column, row: row)
@@ -106,42 +106,42 @@ public class Simulation {
     
     /// Speed adjusts how many times per second the simulation updates.
     public var speed: Int = 2
-        
-    /** 
+    
+    /**
      The user will set this function in the playground page and then during
      the update we will call it.
-    */
+     */
     public var configureCell: ((_ cell: Cell) -> Void)?
     
     /// Initializes a new simulation.
     public init() {
         self.cellSize = CGSize(width: 32, height: 32)
     }
-
+    
     /// Displays an emoji or letters on all cells of the chosen state.
     public func set(_ text: String, forState state: State) {
         
-
+        
         let sourceCharacter = text as NSString
         /*
-         This is an approximation of the appropriate font size based on the cell 
+         This is an approximation of the appropriate font size based on the cell
          dimension, which is always constrainted between 4 and 512.
-        */
+         */
         let font = UIFont.systemFont(ofSize: cellSize.width)
-        let attributes = [NSFontAttributeName : font,
-                          NSForegroundColorAttributeName: UIColor.white]
-        var textSize = sourceCharacter.size(attributes: attributes)
+        let attributes = [NSAttributedString.Key.font : font,
+                          NSAttributedString.Key.foregroundColor: UIColor.white]
+        var textSize = sourceCharacter.size(withAttributes: attributes)
         if textSize.width < 1 || textSize.height < 1 {
             textSize = cellSize
         }
-
+        
         UIGraphicsBeginImageContextWithOptions(textSize, false, 0.0)
         
         sourceCharacter.draw(in: CGRect(x:0, y:0, width:textSize.width,  height:textSize.height), withAttributes: attributes)
         cellConfigurator.set( UIGraphicsGetImageFromCurrentImageContext()!, forState:  state)
         
         UIGraphicsEndImageContext()
-
+        
     }
     
     /// Displays an image on all cells of the chosen state.
@@ -154,3 +154,4 @@ public class Simulation {
         cellConfigurator.set(color, forState: state)
     }
 }
+
